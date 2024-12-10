3428ffddbb29356892e95faf6e8954aac1ef79a6
ZacharyQuizon <zachquizon@gmail.com>
2024-12-10 13:55:50 -0500

    page stuff

--- app.js
+++ app.js
@@ -6,15 +6,25 @@ app.get('/', (req,res) => { //Routes HTTP GET requests to the specified path, as
     res.send('Welcome to the homepage')    //sends "welcome to the home page" as a response to the client
 })
 
-app.get('/signup')
+app.get('/signup', (req,res) => {
+    res.send('this is the signup page');
+});
 
-app.get('/login')
+app.get('/login', (req,res) => {
+    res.send('this is the login page');
+});
 
-app.get('/dashboard')
+app.get('/dashboard', (req,res) => {
+    res.send('this is the dashboard page');
+});
 
-app.get('/transactions')
+app.get('/transactions', (req,res) => {
+    res.send('this is the transactions page');
+});
 
-app.get('/logout')
+app.get('/logout', (req,res) => {
+    res.send('this is the logout page');
+});
 
 app.listen(port,() => { //starts the server on the previously set port
     console.log(`Example app listening on ${port}`) //the callback function logs a message when the server starts successfully.
